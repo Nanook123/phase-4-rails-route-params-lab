fc87fc1d3419461e99c0ee1abe38b23e3752fa2a
Joshua Allford <joshallfordnanook@gmail.com>
2021-11-02 02:57:57 +0000

    commit tree bd96b0f756da945ed1af7bea8c88ae92d2e47cd8

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,16 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = if params[:dickquibbles]
+      Student.by_name(params[:dickquibbles])
+    else
+      Student.all
+      
+  end
+  render json: students
+end
+  def show
+    students = Student.find(params[:id])
     render json: students
   end
 
